173dd05726339b35ce77fd5f55dffd177c2fc453
Ryan Roat <ryanroat@gmail.com>
2020-10-04 14:37:38 -0400

    send sms test app

---
+++ send-sms.js
@@ -0,0 +1,20 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNum = process.env.TWILIO_PHONE_NUM;
+const targetNum = process.env.TARGET_PHONE_NUM;
+
+const smsBody = 'Another test message.';
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+  to: targetNum,
+  from: twilioNum,
+  body: `${smsBody}`,
+})
+  .then((message) => console.log(message))
+  .catch((err) => console.error(err));
