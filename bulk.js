a195ab4e97cf3608401a6d295f426b35b9c0dfcb
Ryan Roat <ryanroat@gmail.com>
2020-10-08 14:26:38 -0400

    read and write target nums to local file

--- bulk.js
+++ bulk.js
@@ -1,14 +1,25 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 require('dotenv').config();
+const fs = require('fs');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const messageDis = process.env.TWILIO_MESSAGING_SERVICE_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNum = process.env.TWILIO_PHONE_NUM;
-const targetNums = [process.env.TARGET_PHONE_NUM]; // array of target phone numbers for smsBody
+// const targetNums = [process.env.TARGET_PHONE_NUM, '+12157688479']; // array of target phone numbers for smsBody
 
 const smsBody = 'Monday night test message.';
+
+// test code to save targetNums to local file
+const util = require('util');
+
+// fs.writeFileSync('./data.json', targetNums.toString(), 'utf-8');
+
+// test code to read targetNums array from local file
+const targetNums = fs.readFileSync('./data.json', 'utf8').split(',');
+// tn = JSON.parse(targetNums);
+console.log(targetNums);
 
 // const client = require('twilio')(accountSid, authToken);
 
